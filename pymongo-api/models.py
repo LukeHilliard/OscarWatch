b9f42eddc8aa04af46a7f93fe306fa8de04815ea
LukeHilliard <119594459+LukeHilliard@users.noreply.github.com>
2024-12-21 21:06:27 +0000

    Created routes for users model/ changed modal to model
    Following the tutorial I have linked at the top of each file, I
    have now created some routes for the User model that allow me to
    create read update and delete a user. The next stage is to
    register these endpoints

--- pymongo-api/models.py
+++ pymongo-api/models.py
@@ -1,29 +1,32 @@
+# To create this I followed this tutorial - https://www.mongodb.com/resources/languages/pymongo-tutorial
 import uuid
 from typing import Optional
 from pydantic import BaseModel, Field
 
 class User(BaseModel):
     id: str = Field(default_factory=uuid.uuid4, alias="_id")
+    user_id: str = Field(...)
     name: str = Field(...)
     password: str = Field(...)
     token: str = Field(...)
-    login: int = Field(...)
-    read_access: int = Field(...)
-    write_access: int = Field(...)
-    is_admin: int = Field(...)
+    login: str = Field(...)
+    read_access: str = Field(...)
+    write_access: str = Field(...)
+    is_admin: str = Field(...)
 
     class Config: 
         allow_population_by_field_name = True
         schema_extra = {
             "example": {
                 "_id": "066de609-b04a-4b30-b46c-32537c7f1f6e",
+                "user_id": "5",
                 "name": "Luke",
                 "password": "wijpyf-pYjbu3-pesmep",
                 "token": "eyJhbGciOiJSUzI1NiIsImtpZCI6IjUxZDcxYzE3MjhjZTgyNDNiZmM5ZWU2NWFjYjhkMTI3NThjZmViOTgiLCJ0eXAiOiJKV1QifQ...",
-                "login": 0,
-                "read_access": 1,
-                "write_access": 1,
-                "is_admin": 1,
+                "login": "0",
+                "read_access": "1",
+                "write_access": "1",
+                "is_admin": "1",
             }
         }
 
@@ -39,12 +42,13 @@ class UserUpdate(BaseModel):
         schema_extra = {
             "example": {
                 "_id": "066de609-b04a-4b30-b46c-32537c7f1f6e",
+                "user_id": "5",
                 "name": "Luke Hilliard",
                 "password": "wijpyf-pYjbu3-pesmep",
                 "token": "eyJhbGciOiJSUzI1NiIsImtpZCI6IjUxZDcxYzE3MjhjZTgyNDNiZmM5ZWU2NWFjYjhkMTI3NThjZmViOTgiLCJ0eXAiOiJKV1QifQ...",
-                "login": 1,
-                "read_access": 1,
-                "write_access": 0,
-                "is_admin": 0,
+                "login": "1",
+                "read_access": "1",
+                "write_access": "0",
+                "is_admin": "0",
             }
         }
